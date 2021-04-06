fa9d7df78c5f6bee7ae247f688a4faf2fc72ced5
dm <dmitrij.demidovich@gmail.com>
2021-04-06 10:46:06 +0300

    git

---
+++ tests/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Unit\Shared\ApiQueryLight;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function test_command()
+    {
+        $this->assertTrue(true);
+    }
+}
