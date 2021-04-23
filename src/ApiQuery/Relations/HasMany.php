b32787914e8890d61735d91c7eb17d3ebc0a661c
dm <dmitrij.demidovich@gmail.com>
2021-04-23 08:37:50 +0300

    added more test coverage

--- src/ApiQuery/Relations/HasMany.php
+++ src/ApiQuery/Relations/HasMany.php
@@ -6,14 +6,13 @@ class HasMany extends HasOne
 {
     protected function relatedDataByKey($relatedData)
     {
-        $relationKey = $this->relationKey;
         $results = [];
+        $relationKey = $this->relationKey;
 
         foreach ($relatedData as $row) {
-            if (empty($row->$relationKey)) {
-                continue;
+            if (! empty($row->$relationKey)) {
+                $results[$row->$relationKey][] = $row;
             }
-            $results[$row->$relationKey][] = $row;
         }
 
         return $results;
